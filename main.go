ee2c179626f2548031c631e016deaec4130f75f5
Paweł Bogaczewicz <pawel.bogaczewicz@outlook.com>
2021-06-07 10:51:46 +0200

    some basic css styling

--- main.go
+++ main.go
@@ -2,35 +2,16 @@ package main
 
 import (
 	"fmt"
-	"html/template"
 	"net/http"
 )
 
 const portNumber = ":8080"
-
-// Home is the handler for the home page
-func Home(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "home.page.tmpl")
-}
-
-// About is the handler for the about page
-func About(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "about.page.tmpl")
-}
-
-func renderTemplate(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
-	err := parsedTemplate.Execute(w, nil)
-	if err != nil {
-		fmt.Println("error parsing template:", tmpl)
-	}
-}
 
 // main is the main function
 func main() {
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
 
-	fmt.Printf("Staring application on port %s", portNumber)
+	fmt.Printf("Starting application on address %s", portNumber)
 	_ = http.ListenAndServe(portNumber, nil)
 }
