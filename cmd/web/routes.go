edc2ae05da2dbc46b362f1dca3927a22126176d4
Paweł Bogaczewicz <pawel.bogaczewicz@outlook.com>
2021-06-08 10:08:30 +0200

    swapping router to chi

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,15 +1,17 @@
 package main
 
 import (
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/lejzab/gintutor/pkg/config"
 	"github.com/lejzab/gintutor/pkg/handlers"
 	"net/http"
 )
 
 func routes(app *config.AppConfig) http.Handler {
-	mux := pat.New()
-	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	mux := chi.NewRouter()
+	mux.Use(middleware.Recoverer)
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
 	return mux
 }
