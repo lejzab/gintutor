825d0238a8b2e76022f88b6d91fdea117a87e65c
Paweł Bogaczewicz <pawel.bogaczewicz@outlook.com>
2021-06-08 10:26:10 +0200

    adds middleware.

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -11,6 +11,7 @@ import (
 func routes(app *config.AppConfig) http.Handler {
 	mux := chi.NewRouter()
 	mux.Use(middleware.Recoverer)
+	mux.Use(NoSurf)
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
 	return mux
